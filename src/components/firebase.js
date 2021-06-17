0f36e14c1225053b243c79b2539f51d159e4e120
madhur <madhurrajput1010@gmail.com>
2021-06-17 16:38:33 +0530

    updated config

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+  }; 
 
   firebase.initializeApp(firebaseConfig);
   export const auth = firebase.auth()
